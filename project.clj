72d55ab2d02474b03c6eb33dc6dc58a69481cbe9
Clement Trosa <me@trosa.io>
2018-01-11 17:24:23 +0100

    Derp

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject quadraduoclj "0.1.0-alpha"
+  :description "42.fr API wrapper"
+  :url "https://github.com/iomonad/42clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ test/quadraduoclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns quadraduoclj.core-test
+  (:require [clojure.test :refer :all]
+            [quadraduoclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
