e5fd4f32f505fa3c33d8f775204bcf212be3ddbd
Clement Trosa <me@trosa.io>
2018-01-26 11:00:34 +0100

    (c) Microsoft 1988

--- test/quadraduoclj/request_test.clj
+++
@@ -1,9 +0,0 @@
-(ns quadraduoclj.request-test
-  (:require [clojure.test :refer :all]
-            [clj-http.client :as client]))
-
-(deftest request-test
-  (testing "Testing request"
-    (let [hc (client/get "https://api.intra.42.fr/v2/topics/4242/messages"
-                         {:content-type :json})]
-      (is (= (:error hc) "Not authorized")))))
